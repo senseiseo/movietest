95e61033046ac0d95ef189bed04790f43aa7765e
Senseiseo <1919mur@gmail.com>
2021-12-09 18:44:10 +0300

    add new model movie

---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie < ApplicationRecord
+  validates :title, presence: true, length: {minimum:2}
+  validates :body, presence: true, length: {minimum:2}
+
+  def created_at_formatted 
+    created_at.strftime('%Y-%m-%d %H:%M:%S')
+  end 
+end 
