b7fb7cbf6dac04f2f3b16ccb5cbeeeb9350a549d
Senseiseo <1919mur@gmail.com>
2021-12-16 14:43:21 +0300

    add remember me

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,11 +2,26 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
-
     private
 
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present?
+      user = session[:user_id].present? ? user_from_session : user_from_token
+
+      @current_user ||= user&.decorate
+    end
+
+    def user_from_session
+      User.find_by(id: session[:user_id])
+    end
+
+    def user_from_token
+      user = User.find_by(id: cookies.encrypted[:user_id])
+      token = cookies.encrypted[:remember_token]
+
+      return unless user&.remember_token_authenticated?(token)
+
+      sign_in user
+      user
     end
   
     def user_signed_in? 
@@ -31,7 +46,20 @@ module Authentication
       session[:user_id] = user.id
     end
 
+    def remember(user)
+      user.remember_me
+      cookies.encrypted.permanent[:remember_token] = user.remember_token
+      cookies.encrypted.permanent[:user_id] = user.id
+    end 
+
+    def forget(user)
+      user.forget_me
+      cookies.delete :user_id
+      cookies.delete :remember_token
+    end 
+
     def sign_out 
+      forget current_user
       session.delete :user_id
       @current_user = nil
     end
